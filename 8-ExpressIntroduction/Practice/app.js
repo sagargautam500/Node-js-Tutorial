0c1320717d7c9caf1325848651351efa2e28e771
Sagar Gautam <sagargautam389@gmail.com>
2025-03-19 10:12:36 +0545

    question

--- 8-ExpressIntroduction/Practice/app.js
+++ 8-ExpressIntroduction/Practice/app.js
@@ -1,3 +1,14 @@
+// QUESTION FOR PRACTICE::
+// Create a new project.
+// 1. Install nodemon and express.
+// 2. Add two dummy middleware that logs request path and request method respectively.
+// 3. Add a third middleware that returns a response.
+// 4. Now add handling using two more middleware that handle path /, a request to /contact-us page.
+// 5. Contact us should return a form with name and email as input fields that submits to /contact-us page also. 
+// 6. Also handle POST incoming request to /contact-us path using a separate middleware.
+
+
+
 const express=require('express');
 
 const app=express();
