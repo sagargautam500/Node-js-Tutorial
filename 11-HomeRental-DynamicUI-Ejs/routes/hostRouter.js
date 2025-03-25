ef86100ffc08c14cabca68ecc834750244202a70
Sagar Gautam <sagargautam389@gmail.com>
2025-03-25 15:46:08 +0545

    addCode

--- 11-HomeRental-DynamicUI-Ejs/routes/hostRouter.js
+++ 11-HomeRental-DynamicUI-Ejs/routes/hostRouter.js
@@ -12,15 +12,17 @@ const hostRouter = express.Router();
 hostRouter.get("/add-home", (req, res, next) => {
   // res.sendFile(path.join(__dirname, "../", "views", "addHome.html"));
   // res.sendFile(path.join(rootDir, "views", "addHome.html"));
-  res.render('addHome',{pageTitle:'addHome'})
+  res.render('addHome',{pageTitle:'addHome',currentPage:'addHome'})
 });
 
 const registerHome=[];
 
 hostRouter.post("/add-home", (req, res, next) => {
-  const houseName=req.body.houseName;
-  registerHome.push({houseName:houseName,})
-  res.render('addedHome',{houseName,pageTitle:'Result'})   //send ejs file
+  // const houseName=req.body.houseName;
+  // registerHome.push({houseName:houseName,})
+  registerHome.push(req.body) //push js object 
+
+  res.render('addedHome',{registerHome:req.body,pageTitle:'Result',currentPage:'addHome'})   //send ejs file
   // res.sendFile(path.join(rootDir, "views", "addedHome.html")); //send html file
 });
 exports.hostRouter = hostRouter;
