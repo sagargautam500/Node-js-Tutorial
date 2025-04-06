8593e5b62a7bb806552048406716f2fb51836403
Sagar Gautam <sagargautam389@gmail.com>
2025-04-06 07:53:43 +0545

    mvc

--- 12-MVC/models/home.js
+++ 12-MVC/models/home.js
@@ -1,1 +1,16 @@
 //fake database::
+const registerHome = [];
+
+module.exports=class Home {
+  constructor(houseName,price,location,rating,photoUrl){
+   this.houseName=houseName
+   this.price=price
+   this.location=location
+   this.rating=rating
+   this.photoUrl=photoUrl
+  }
+
+save(){
+  registerHome.push(this)
+}
+}
